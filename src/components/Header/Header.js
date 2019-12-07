a1bfd256fac83aa7b634f161e8884a6a8e3e7969
Brandon LaFave <blafave@hollandamerica.com>
2019-12-07 10:46:55 -0800

    Convert Header component to functional component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,17 +1,15 @@
-import React, { Component } from 'react';
-import logo from '../../assets/images/logo.svg';
+import React from 'react';
+import logo from '../../assets/images/mtg-logo.svg';
 import './Header.css';
 
-class Header extends Component {
-  render() {
-    return (
-      <div className="header-wrapper">
-        <div className="header-logo-wrapper">
-          <img src={logo} className="header-logo" alt="logo" />
-        </div>
+function Header(props) {
+  return (
+    <div className="header-wrapper">
+      <div className="header-logo-wrapper">
+        <img src={logo} className="header-logo" alt="logo" />
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default Header;
