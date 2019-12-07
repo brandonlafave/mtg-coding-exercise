5f7ec94ec24d2c7c482929bfe9fab3451daa9d1d
Brandon LaFave <blafave@hollandamerica.com>
2019-12-06 19:41:55 -0800

    Update naming conventions in index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './pages/Home';
+import Home from './pages/Home';
 import './index.css';
 
 ReactDOM.render(
-  <App />,
+  <Home />,
   document.getElementById('root')
 );
