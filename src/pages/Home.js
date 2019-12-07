5643d64b5128146d9c1495ad45a2db2736ec4a80
Brandon LaFave <blafave@hollandamerica.com>
2019-12-06 19:40:34 -0800

    Update naming conventions used in the Home and Header components

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react';
 import Header from '../components/Header'
+import Footer from '../components/Footer'
 import './Home.css';
 
-class App extends Component {
+class Home extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="home-wrapper">
         <Header />
+        <div>Hello world</div>
+        <Footer />
       </div>
     );
   }
 }
 
-export default App;
+export default Home;
