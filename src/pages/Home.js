d6857e60e02c988c57c2a15a834def8a3db03227
Brandon LaFave <blafave@hollandamerica.com>
2019-12-09 06:41:55 -0800

    Add infinite scroll to project

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,8 +3,9 @@ import Header from '../components/Header';
 import Footer from '../components/Footer';
 import Card from '../components/Card';
 import Loader from '../components/Loader';
+import Copy from '../components/Copy';
 import fetchData from '../utilities/fetchData';
-
+import _debounce from "lodash.debounce";
 import './Home.css';
 
 class Home extends Component {
@@ -13,39 +14,106 @@ class Home extends Component {
 
     this.state = {
       cardList: [],
-      isLoading: true
+      maximumCardsPerPage: 100,
+      cardsFromCurrentPage: 0,
+      isLoading: true,
+      requestParams: {
+        page: 1,
+        pageSize: 20,
+        type: 'creature',
+        orderBy: 'name'
+      }
+    }
+
+    window.onscroll = () => {
+      this.infiniteScrollRequest(
+        window.pageYOffset,
+        window.innerHeight,
+        this.home.offsetHeight
+      );
     }
   }
 
   componentDidMount() {
-   fetchData(20, 'name', 'creature')
-   .then(response => {
-    this.setState({
-      cardList: response,
-      isLoading: false
-    })
-   });
+    this.getCards();
   }
 
-  generateCardTiles(cardList) {
+  /**
+   * generateCardTiles - Generates the HTML for the card tiles
+   * @param {array} cardList An array containing the card values
+   * @return {JSX} Returns a list of Card components
+   */
+  generateCardTiles = (cardList) => {
     if (Array.isArray(cardList) && cardList.length > 0) {
-      return cardList.map((card) => {
-        return <Card key={ card.id } { ...card } />
+      return cardList.map((card, index) => {
+        return <Card key={ `${card.id}-${index}` } { ...card } />
       });
     } else {
       return;
     }
   };
 
+  /**
+   * infiniteScrollRequest - Makes an additional API request if the user has reached the bottom of the page
+   * @param {array} response An array containing the card values
+   * @param {string} pageYOffset The window.pageYOffset value
+   * @param {string} innerHeight The window.innerHeight
+   * @param {string} homeWrapperOffset The offset values for the home wrapper
+   */
+  infiniteScrollRequest = (pageYOffset, innerHeight, homeWrapperOffset) => {
+    const hasReachedEndOfPage = pageYOffset + innerHeight === homeWrapperOffset && !this.state.isLoading;
+
+    return hasReachedEndOfPage ? this.getCards() : null;
+  };
+
+  getCards = () => {
+    this.setState({
+      isLoading: true
+    }, () => {
+      fetchData(this.state.requestParams)
+      .then(response => {
+        this.handleCardResponse(response);
+      })
+    })
+  }
+
+  /**
+   * handleCardResponse - Handles the card response
+   * @param {array} response An array containing the card values
+   */
+  handleCardResponse = (response) => {
+    let requestParams = this.state.requestParams,
+        cardList = this.state.cardList,
+        page = requestParams.page;
+
+    if (Array.isArray(response) && response.length > 0) {
+      cardList = [ ...cardList, ...response ];
+      page += 1;
+
+      requestParams.page = page;
+    }
+
+    this.setState({
+      requestParams,
+      cardList,
+      isLoading: false
+    })
+  };
+
   render() {
     let { cardList, isLoading } = this.state;
+    const title = 'Magic the Gathering Coding Exercise';
+    const copy = 'Keep scrolling to load more cards!';
 
     return (
-      <div className="home-wrapper">
+      <div className="home-wrapper" ref={el => this.home = el}>
         <Header />
         <main className='body-content' role='main'>
-            <Loader isLoading={isLoading} />
+          <Copy title={title} copy={copy} />
+          <div className='card-tile-container'>
             { this.generateCardTiles(cardList) }
+          </div>
+          <Loader isLoading={isLoading} />
         </main>
         <Footer />
       </div>
--- src/pages/Home.test.js
+++ src/pages/Home.test.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Home from './Home';
 import { mount } from 'enzyme';
-import { SAMPLE_CARD } from '../components/Card/constants';
+import { CARD_ONE_LIST, CARD_TWO_LIST } from '../components/Card/constants';
 
 describe('<Home />', () => {
-  let wrapper = mount(<Home />);
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = mount(<Home />);
+  });
 
   describe('generateCardTiles Tests', () => {
     it('should not display cards if the card list is an empty array', () => {
@@ -20,14 +24,66 @@ describe('<Home />', () => {
     });
 
     it('should return the card tiles if an array ', () => {
-      let cardList = [];
-          cardList[0] = SAMPLE_CARD;
-
       wrapper.setState({
-        cardList
+        cardList: CARD_ONE_LIST
       });
 
       expect(wrapper.find('.card-name td').text()).toEqual('Abattoir Ghoul');
+    });
+  });
+
+  describe('handleCardResponse Tests', () => {
+    beforeEach(() => {
+      wrapper.setState({
+        cardList: CARD_ONE_LIST
+      });
+    });
+
+    it('should turn the loader off after handling the response', () => {
+        wrapper.instance().handleCardResponse(CARD_ONE_LIST);
+
+        expect(wrapper.state('isLoading')).toEqual(false);
+    });
+
+    it('should concatenate the response with the cards list when the response is an array', () => {
+      wrapper.instance().handleCardResponse(CARD_TWO_LIST);
+
+      expect(wrapper.state('cardList').length).toEqual(2);
+    });
+
+    it('should increment the page value if the response is an array with at least one value', () => {
+      wrapper.instance().handleCardResponse(CARD_TWO_LIST);
+
+      expect(wrapper.state('requestParams')['page']).toEqual(2);
+    });
+
+    it('should not concatenate the response with the cards list when the response is not an array', () => {
+      wrapper.instance().handleCardResponse({});
+
+      expect(wrapper.state('cardList').length).toEqual(1);
+    });
+
+    it('should not increment the page value if the response is not an array with at least one value', () => {
+      wrapper.instance().handleCardResponse({});
+      expect(wrapper.state('requestParams')['page']).toEqual(1);
+    });
+  });
+
+  describe('infiniteScrollRequest Tests', () => {
+    it('should return null if the user has not reached the bottom of the page', () => {
+      const infiniteScrollRequest = wrapper.instance().infiniteScrollRequest(1500, 300, 2000);
+
+      expect(infiniteScrollRequest).toEqual(null);
+    });
+
+    it.skip('should call getCards one time once when the user has reached the bottom of the page', () => {
+      const spy = jest.spyOn(wrapper.instance(), 'getCards');
+
+      for (let i=0; i < 5; i++) {
+        wrapper.instance().infiniteScrollRequest(1500, 500, 2000);
+      }
+
+      expect(spy).toHaveBeenCalledTimes(1);
     });
   });
 });
