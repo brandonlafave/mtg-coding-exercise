36f8197bf70c0a86021c12cd094a7238884f1e30
Brandon LaFave <blafave@hollandamerica.com>
2019-12-09 17:42:38 -0800

    Update Copy component to support padding custom CSS classname

--- src/components/Copy/Copy.js
+++ src/components/Copy/Copy.js
@@ -2,10 +2,10 @@ import React from 'react';
 import './Copy.css';
 
 function Copy(props) {
-  const { title, copy } = props;
+  const { title, copy, copyClassName } = props;
 
   return (
-    <section className="copy-wrapper">
+    <section className={`copy-wrapper ${copyClassName}`}>
       { title ? <h1 className="copy-title">{title}</h1> : null }
       { copy ? <p className="copy-text">{copy}</p> : null }
     </section>
