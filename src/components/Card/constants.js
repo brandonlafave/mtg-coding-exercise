5fb4d3550ebe0bd5778af0ed213488c22241055b
Brandon LaFave <blafave@hollandamerica.com>
2019-12-09 17:43:35 -0800

    Update constants file to change the SET_LABEL value from Set to Set Name so it's clearer

--- src/components/Card/constants.js
+++ src/components/Card/constants.js
@@ -2,7 +2,7 @@
 export const NO_INFO_AVAILABLE = 'No info is available';
 export const NAME_LABEL = 'Name:';
 export const ARTIST_LABEL = 'Artist:';
-export const SET_LABEL = 'Set:';
+export const SET_LABEL = 'Set Name:';
 export const ORIGINAL_TYPE_LABEL = 'Type:';
 export const DEFAULT_IMAGE_URL = 'https://via.placeholder.com/223x310/808080/FFFFFF?text=No+image+available';
 export const SAMPLE_CARD = {
